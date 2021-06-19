12e746890d52c82c342809c70034432f9d6cdd4b
Eric <eric.m.lombardo@gmail.com>
2021-06-19 15:29:05 -0400

    Add attributes to post and comment tables

--- db/migrate/20210619190902_create_posts.rb
+++ db/migrate/20210619190902_create_posts.rb
@@ -2,7 +2,11 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.string :title
+      t.string :author
+      t.string :published
       t.text :content
+      t.integer :likes
+      t.integer :views
 
       t.timestamps
     end
