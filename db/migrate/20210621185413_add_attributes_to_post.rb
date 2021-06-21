a82923d0f21155e6a8349cfafd24da6609e9cf99
Eric <eric.m.lombardo@gmail.com>
2021-06-21 15:21:14 -0400

    Add title_pic and like_pic to post table

--- db/migrate/20210621185413_add_attributes_to_post.rb
+++
@@ -1,6 +0,0 @@
-class AddAttributesToPost < ActiveRecord::Migration[6.1]
-  def change
-    add_column :posts, :title_pic, :string
-    add_column :posts, :like_pic, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_185413) do
+ActiveRecord::Schema.define(version: 2021_06_19_191456) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -27,10 +27,10 @@ ActiveRecord::Schema.define(version: 2021_06_21_185413) do
     t.text "content"
     t.integer "likes"
     t.integer "views"
+    t.string "like_pic"
+    t.string "title_pic"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "title_pic"
-    t.string "like_pic"
   end
 
 end
