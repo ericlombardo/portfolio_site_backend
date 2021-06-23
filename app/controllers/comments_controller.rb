a57210b9ad722abbdfedefdcdb3891e800c1a83c
Eric <eric.m.lombardo@gmail.com>
2021-06-23 15:08:34 -0400

    Change comment to instance variables

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,46 +3,46 @@ class CommentsController < ApplicationController
 
   # GET /comments
   def index
-    comments = Comment.all
+    @comments = Comment.all
 
-    render json: comments
+    render json: @comments
   end
 
   # GET /comments/1
   def show
-    comment = Comment.find_by(id: params[:id])
-    render json: comment
+    @comment = @comment.find_by(id: params[:id])
+    render json: @comment
   end
 
   # POST /comments
   def create
-    comment = Comment.new(comment_params)
+    @comment = Comment.new(comment_params)
 
-    if comment.save
-      render json: comment, status: :created, location: comment
+    if @comment.save
+      render json: @comment, status: :created, location: @comment
     else
-      render json: comment.errors, status: :unprocessable_entity
+      render json: @comment.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /comments/1
   def update
-    if comment.update(comment_params)
-      render json: comment
+    if @comment.update(comment_params)
+      render json: @comment
     else
-      render json: comment.errors, status: :unprocessable_entity
+      render json: @comment.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /comments/1
   def destroy
-    comment.destroy
+    @comment.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
-      comment = Comment.find(params[:id])
+      @comment = Comment.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
