2b6517b81c97106cb87d24ad23964f94426165fc
Eric <eric.m.lombardo@gmail.com>
2021-06-23 14:51:42 -0400

    Add to strong params and make post => @post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,50 +3,50 @@ class PostsController < ApplicationController
 
   # GET /posts
   def index
-    posts = Post.all
+    @posts = Post.all
 
-    render json: posts
+    render json: @posts
   end
 
   # GET /posts/1
   def show
-    post = Post.find_by(id: params[:id])
-    render json: post
+    @post = Post.find_by(id: params[:id])
+    render json: @post
   end
 
   # POST /posts
   def create
-    post = Post.new(post_params)
+    @post = Post.new(post_params)
 
     if post.save
-      render json: post, status: :created, location: post
+      render json: @post, status: :created, location: @post
     else
-      render json: post.errors, status: :unprocessable_entity
+      render json: @post.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /posts/1
   def update
-    if post.update(post_params)
-      render json: post
+    if @post.update(post_params)
+      render json: @post
     else
-      render json: post.errors, status: :unprocessable_entity
+      render json: @post.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /posts/1
   def destroy
-    post.destroy
+    @post.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      post = Post.find(params[:id])
+      @post = Post.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :author, :published, :content, :likes, :views, :title_pic, :like_pic)
     end
 end
