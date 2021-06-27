f94ace1c0da12c9cbb3404edbc712bbceb24bbdc
Eric <eric.m.lombardo@gmail.com>
2021-06-26 21:35:15 -0400

    Add create route to nested comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :comments
   resources :posts do
-    resources :comments, only: [:index]
+    resources :comments, only: [:index, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
